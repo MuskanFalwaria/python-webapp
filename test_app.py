2ff5417688a7a9f9f6ef20ad25f501525a488ad3
MuskanFalwaria <113618140+MuskanFalwaria@users.noreply.github.com>
2025-04-04 15:52:53 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,8 @@
-# test_app.py
+from app import app
 
-def test_dummy():
-    assert 1 == 1
+def test_home():
+    tester = app.test_client()
+    response = tester.get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
+
